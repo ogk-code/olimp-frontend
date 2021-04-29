9e533a03f3a63dd6b8b881ea2b7b766953b36b8a
31Vector31 <64965701+31Vector31@users.noreply.github.com>
2021-04-29 18:50:41 +0300

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <form class="login" @submit.prevent="login">
-      <h3 align="center">Введіть ID та пароль, який було выслано вам на email. Якщо данні не вірні або або лист за
-        данними не прийшов зверніться до слежби підтримки</h3>
+      <h3 align="center">Введіть ID і пароль, які були відправлені вам на email. Якщо дані не вірні - повідомте в службу підтримки.</h3>
       <!--<label>Email</label>
       <input required v-model="email" type="email" placeholder="Name"/>
       <label>Password</label>
